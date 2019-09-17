c33a5a53bb2a3f6a49ff9bc76aefc55da28d7217
emiliovasconcelos <antonioemiliopv@gmail.com>
2019-09-17 15:57:08 -0300

    inclui controles de erros e finalizei o simulador

--- js/simulador.js
+++ js/simulador.js
@@ -3,6 +3,14 @@ var botaoCalcular = document.querySelector("#realizar-calculo");
 botaoCalcular.addEventListener("click", function(event) {
     event.preventDefault();
 
+    var limparTabelaResultado = document.querySelector("#tabela-resultado");
+    var limparMensagemErro = document.querySelector("#mensagem-erro");
+    var limparNotaContribuicao = document.querySelector("#nota-contribuicao");
+    
+    //limpar tela
+    limparTabelaResultado.innerHTML = "";
+    limparMensagemErro.innerHTML = "";
+
     //recebendo os valores do formulário
     var dataNascimento = new Date(document.getElementById("data-nascimento").value);
     var sexo = document.getElementById("sexo").value;
@@ -12,11 +20,65 @@ botaoCalcular.addEventListener("click", function(event) {
     var contribuicaoEspontanea = document.getElementById("contribuicao-espontanea").value;
     var rentabilidadeEsperada = document.getElementById("rentabilidade-esperada").value;
     var prazoRecebimento = document.getElementById("prazo-recebimento").value;
-
+    
     //calculando a idade
     var dataSimulacao = new Date();
     var idadeAtualEmMeses = Math.trunc((dataSimulacao.getTime() - dataNascimento.getTime()) / (1000 * 3600 * 24 * 365.25) * 12);
     
+    //tratamento de erros
+    var erros = [];
+    
+    if (dataNascimento >= dataSimulacao) {
+        erros.push("Data de nascimento maior ou igual a data atual");
+        document.getElementById("data-nascimento").focus();
+    }
+
+    if (sexo == 1 && idadeAposentadoria < 53) {
+        erros.push("A idade mínima de aposentadoria para homens é 53 anos");
+        document.getElementById("idade-aposentadoria").focus();
+    }
+
+    if (sexo == 2 && idadeAposentadoria < 48) {
+        erros.push("A idade mínima de aposentadoria para mulheres é 48 anos");
+        document.getElementById("idade-aposentadoria").focus();
+    }
+
+    if (salario < 0) {
+        erros.push("Salário não pode ser negativo");
+        document.getElementById("salario-real-contribuicao").focus();
+    }
+
+    if (aporte < 0) {
+        erros.push("Aporte Inicial não pode ser menor que zero");
+        document.getElementById("aporte-inicial").focus();
+    }
+
+    if (contribuicaoEspontanea < 0) {
+        erros.push("Contribuição Espontânea não pode ser menor que zero");
+        document.getElementById("contribuicao-espontanea").focus();
+    }
+
+    if (rentabilidadeEsperada < 0 || rentabilidadeEsperada > 6) {
+        erros.push("Rentabilidade esperada deve ser um valor entre 0 e 6");
+        document.getElementById("rentabilidade-esperada").focus();
+    }
+
+    if (prazoRecebimento < 10 || prazoRecebimento > 50) {
+        erros.push("Prazo de recebimento deve ser um valor entre 10 e 50");
+        document.getElementById("prazo-recebimento").focus();
+    }
+
+    if (erros.length > 0) {
+        var ulMensagemErro = document.querySelector("#mensagem-erro");
+        ulMensagemErro.innerHTML = "";
+
+        erros.forEach(function(erro){
+            var li = document.createElement("li");
+            li.textContent = erro;
+            ulMensagemErro.appendChild(li);
+        });
+    }
+
     //calculando o tempo de acumulação em meses
     var tempoAcumulacaoReservaEmMeses = idadeAposentadoria * 12 - idadeAtualEmMeses;
 
@@ -106,42 +168,78 @@ botaoCalcular.addEventListener("click", function(event) {
     
     //criando td para contribuição
     var tdTextoContribuicao = document.createElement("td");
-    tdTextoContribuicao.textContent = "Contribuição simulada";
+    tdTextoContribuicao.textContent = "Contribuição mensal: ";
     tdTextoContribuicao.classList.add("resultado-contribuicao-texto");
     var tdValorContribuicao = document.createElement("td");
-    tdValorContribuicao.textContent = contribuicaoParticipante.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    tdValorContribuicao.textContent = contribuicaoTotal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
     tdValorContribuicao.classList.add("resultado-contribuicao-valor");
 
+    //criando td para reserva acumulada
+    var valorReserva = vetorResultado[vetorResultado.length-1][9]
+    var tdTextoReserva = document.createElement("td");
+    tdTextoReserva.textContent = "Reserva total acumulada: ";
+    tdTextoReserva.classList.add("resultado-reserva-texto");
+    var tdValorReserva = document.createElement("td");
+    tdValorReserva.textContent = valorReserva.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    tdValorReserva.classList.add("resultado-reserva-valor");
+    
     //criando td para benefício
     var tdTextoBeneficio = document.createElement("td");
-    tdTextoBeneficio.textContent = "Benefício simulado";
+    tdTextoBeneficio.textContent = "Benefício inicial mensal: ";
     tdTextoBeneficio.classList.add("resultado-beneficio-texto");
     var tdValorBeneficio = document.createElement("td");
     tdValorBeneficio.textContent = valorBeneficioSimulacao.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
     tdValorBeneficio.classList.add("resultado-beneficio-valor");
+
+    //criando td para prazo de recebimento
+    var tdTextoPrazoRecebimento = document.createElement("td");
+    tdTextoPrazoRecebimento.textContent = "Prazo de recebimento: ";
+    tdTextoPrazoRecebimento.classList.add("resultado-prazo-recebimento-texto");
+    var tdValorPrazoRecebimento = document.createElement("td");
+    tdValorPrazoRecebimento.textContent = prazoRecebimento + " anos";
+    tdValorPrazoRecebimento.classList.add("resultado-prazo-recebimento-valor");
 
     //criando tr contribuição
     var trContribuicao = document.createElement("tr");
     trContribuicao.appendChild(tdTextoContribuicao);
     trContribuicao.appendChild(tdValorContribuicao);
 
+    //criando tr reserva acumulada
+    var trReserva = document.createElement("tr");
+    trReserva.appendChild(tdTextoReserva);
+    trReserva.appendChild(tdValorReserva);
+    
     //criando tr benefício
     var trBeneficio = document.createElement("tr");
     trBeneficio.appendChild(tdTextoBeneficio);
     trBeneficio.appendChild(tdValorBeneficio);
 
+    //criando tr prazo de recebimento
+    var trPrazoRecebimento = document.createElement("tr");
+    trPrazoRecebimento.appendChild(tdTextoPrazoRecebimento);
+    trPrazoRecebimento.appendChild(tdValorPrazoRecebimento);
+
     //inserindo tr e td na tabela
     var tabela = document.querySelector("#tabela-resultado");
     tabela.appendChild(trContribuicao);
+    tabela.appendChild(trReserva);
     tabela.appendChild(trBeneficio);
+    tabela.appendChild(trPrazoRecebimento);
 
-
-
-
-
-
-
-
-
-
+    //inserindo notas ao resultado da simulação
+    var ulNotaSimulacao = document.querySelector("#nota-contribuicao");
+    ulNotaSimulacao.innerHTML = "";
+    var notaSimulacao = [];
+    notaSimulacao.push("As contribuições para o plano são feitas inclusive sobre o 13º salário");
+    notaSimulacao.push("O benefício simulado considera 12 pagamentos por ano");
+    notaSimulacao.forEach(function(nota){
+        var liNotaSimulacao = document.createElement("li");
+        liNotaSimulacao.textContent = nota;
+        ulNotaSimulacao.appendChild(liNotaSimulacao);
+    });
+    
+    if (erros.length > 0) {
+        limparTabelaResultado.innerHTML = "";
+        limparNotaContribuicao.innerHTML = "";      
+    }
 })
